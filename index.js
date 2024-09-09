8967a338de1828f722bfdb751368f37d4a7b7d98
ranjit1024 <ranjitdas2048@gmail.com>
2024-09-09 21:33:25 +0530

    adding add function

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("working with ")
+const addForm = document.querySelector(".add");
+const taskList = document.querySelector(".tasks");
+addForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const value = addForm.addtask.value.trim();
+    if(value.length){
+        console.log(value);
+        taskList.innerHTML += `<li class="task">
+        <span>${addForm.addtask.value}</span>
+        <i class="bi bi-trash delete"></i>
+        </li>`
+    }
+    addForm.reset();
+})
