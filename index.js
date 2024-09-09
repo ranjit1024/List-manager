d1c0134ee11967fb75c74484d6f4d3aba38b2e75
ranjit1024 <ranjitdas2048@gmail.com>
2024-09-09 22:48:02 +0530

    adding final file

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const addForm = document.querySelector(".add");
 const taskList = document.querySelector(".tasks");
+const clearAll = document.querySelector(".clearall");
+const messageSpan = document.querySelector(".message span");
+const searchForm = document.querySelector("#search");
+
+
+function updateTask(){
+    const allListItems = document.querySelectorAll("li");
+    console.log(allListItems.length);
+    messageSpan.innerText = `you have ${allListItems.length} pending tasks.`
+}
+updateTask();
+
 addForm.addEventListener("submit", (e)=>{
     e.preventDefault();
     const value = addForm.addtask.value.trim();
@@ -11,4 +23,44 @@ addForm.addEventListener("submit", (e)=>{
         </li>`
     }
     addForm.reset();
+    updateTask();
+});
+
+taskList.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("delete")){
+        e.target.parentElement.remove();
+    }
+    updateTask();
+});
+
+clearAll.addEventListener("click",(e)=>{
+    const listItem = document.querySelectorAll("li");
+    listItem.forEach((item)=>{
+        item.remove();
+    });
+    updateTask();
 })
+
+function filterTask(term){
+    const tasklistValues = Array.from(taskList.children)
+    .filter(task => {
+        return !task.textContent.includes(term);
+    })
+    .forEach(element => {
+        element.classList.add("hide");
+    })
+
+    Array.from(taskList.children).
+    filter(task => {
+        return task.textContent.includes(term);
+    }).forEach(elemet => {
+        elemet.classList.remove("hide");
+    })
+}
+
+
+searchForm.task.addEventListener("keyup", (e)=>{
+    const serchValue = searchForm.task.value.trim();
+    filterTask(serchValue);
+});
+
